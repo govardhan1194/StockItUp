d433268c3f4e10500778fb63519245381051e2f9
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-06-11 12:10:51 -0700

    Update Producer.py

--- Pulsar_Producer/Producer.py
+++ Pulsar_Producer/Producer.py
@@ -4,7 +4,7 @@ import pulsar
 
 client = pulsar.Client('pulsar://localhost:6650')
 
-producer = client.create_producer('StockItUp-topic')
+producer = client.create_producer('StockItUp-topic',AvroSchema(Stock))
 
 s3 = boto3.resource('s3', aws_access_key_id = '', aws_secret_access_key = '')
 
@@ -18,7 +18,11 @@ for object in bucket.objects.all():
         
         #read through each line of csv and send the line to the pulsar topic
         for index, row in data.iterrows():
+            '''
             output = ''
             for element in row:
                     output = output + str(element) + ","
             producer.send(output.encode('utf-8'))
+            '''
+            output = Stock(Mnemonic = row["Mnemonic"],SecurityType = row["SecurityType"],Currency = row["Currency"],Date = row["Time"],StartPrice	= row["StartPrice"],MaxPrice = row["MaxPrice"],MinPrice = row["MinPrice"],EndPrice = row["EndPrice"],TradedVolume = row["TradedVolume"],NumberOfTrades = row["NumberOfTrades"])
+            producer.send(output)
