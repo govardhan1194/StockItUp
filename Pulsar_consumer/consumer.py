c31997fcc4ff9eb7372e132871433c09b9841aa6
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-06-15 16:00:12 -0700

    Update consumer.py

--- Pulsar_consumer/consumer.py
+++ Pulsar_consumer/consumer.py
@@ -1,4 +1,6 @@
 import pulsar
+from pulsar.schema import *
+from schemas import Stock
 
 # Create a Pulsar client instance. The instance can be shared across multiple
 # producers and consumers
@@ -6,19 +8,23 @@ client = pulsar.Client('pulsar://localhost:6650')
 
 # Subscribe to the topic. If the topic does not exist, it will be
 # automatically created
-consumer = client.subscribe('StockItUp-topic')
+consumer = client.subscribe(
+    'testnewschema1', 'my-subsciption', schema=AvroSchema(Stock))
 
 while True:
     try:
         # try and receive messages with a timeout of 10 seconds
         msg = consumer.receive(timeout_millis=10000)
-
+        Output = msg.value().__dict__
+        Mnemonic = Output['Mnemonic']
+        print(type(Mnemonic))
         # Do something with the message
-        print("Received message '%s'", msg.data())
+        print("Received message '%s'", msg.value())
 
         # Acknowledge processing of message so that it can be deleted
-        consumer.acknowledge(msg)
+        # consumer.acknowledge(msg)
     except Exception:
         print("No message received in the last 10 seconds")
 
 client.close()
+
