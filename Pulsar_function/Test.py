7c5ef7f65ed6a637d54191ad93a7562bee87fed1
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-06-22 16:20:05 -0700

    Update Test.py

--- Pulsar_function/Test.py
+++ Pulsar_function/Test.py
@@ -1,11 +1,40 @@
 from pulsar import Function
 
-# Example function that uses the built in publish function in the context
-# to publish to a desired topic based on config
+# Function that uses the built in publish function in the context
+# to publish to a desired topic 
+# This function creates a new column called ProfitorLoss
+
 class ProcessData(Function):
   
+  def __init__(self):
+      pass
+    
+  def convert_time(time):
+      time_fmt = "%H:%M"
+      return datetime.strptime(time, time_fmt).strftime(time_fmt)
+
+  def convert_dt(date, time):
+      return datetime.strptime(date + ' ' + time, '%Y-%m-%d %H:%M')
+  
+  # This is the main function which processes the data as it is being received.
+  # Each message while streaming goes through this function and publishes to another topic and is automated.
   def process(self, input, context):
-    publish_topic = "testtopicfunction"
-    context.publish(publish_topic, input)
-    print(input)
-    return
+      publish_topic = "ProcessedData"
+      time_fmt = "%H:%M"
+      
+      data = input.__dict__
+     
+      ProfitorLoss = data['StartProce'] - data['EndPrice']
+      
+      data["Time"] = convert_dt(data["Time"])
+      
+      data['ProfitorLoss'] = ProfitorLoss
+      
+      opening_hours = datetime.strptime("08:00", time_fmt).strftime(time_fmt)
+      closing_hours = datetime.strptime("20:00", time_fmt).strftime(time_fmt)
+      
+      if data["Time"] > opening_hours and data['Time'] < closing_hours:
+      
+        context.publish(publish_topic, data)
+      
+      return
