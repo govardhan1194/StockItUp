cf5d64f4ee7ef8e86c27647ba931329b7ebfd5d1
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-06-22 16:29:19 -0700

    Update consumer.py

--- Pulsar_consumer/consumer.py
+++ Pulsar_consumer/consumer.py
@@ -9,19 +9,17 @@ client = pulsar.Client('pulsar://localhost:6650')
 # Subscribe to the topic. If the topic does not exist, it will be
 # automatically created
 consumer = client.subscribe(
-    'testnewschema1', 'my-subsciption', schema=AvroSchema(Stock))
+    'ProcessedData', 'my-subsciption', schema=AvroSchema(Stock))
 
 while True:
     try:
         # try and receive messages with a timeout of 10 seconds
         msg = consumer.receive(timeout_millis=10000)
-        Output = msg.value().__dict__
-        Mnemonic = Output['Mnemonic']
-        print(type(Mnemonic))
+        
         # Do something with the message
         print("Received message '%s'", msg.value())
 
-        # Acknowledge processing of message so that it can be deleted
+        # Acknowledge processing of message so that it can be deleted, which we wont be doing since we need it in the TOPIC.
         # consumer.acknowledge(msg)
     except Exception:
         print("No message received in the last 10 seconds")
