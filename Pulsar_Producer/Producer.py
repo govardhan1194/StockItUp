e3691e60234ef448e0ba2e3ff7fb5a467c832e45
Govardhan Isayamudhan Naicker <62525512+govardhan1194@users.noreply.github.com>
2020-06-22 16:26:39 -0700

    Update Producer.py

--- Pulsar_Producer/Producer.py
+++ Pulsar_Producer/Producer.py
@@ -1,3 +1,4 @@
+#These are the required modules to be imported.
 import boto3
 import pandas as pd
 import pulsar
@@ -14,6 +15,7 @@ bucket = s3.Bucket('deutscheboercestockitup')
 
 for object in bucket.objects.all():
     
+    #Eliminating empty csv files which are of 136 bytes of size.
     if object.size > 136:
         url = 'https://deutscheboercestockitup.s3-us-west-2.amazonaws.com/' + object.key
         data = pd.read_csv(url)
@@ -26,5 +28,9 @@ for object in bucket.objects.all():
                     output = output + str(element) + ","
             producer.send(output.encode('utf-8'))
             '''
-            output = Stock(Mnemonic = row["Mnemonic"],SecurityType = row["SecurityType"],Currency = row["Currency"],Date = row["Time"],StartPrice	= row["StartPrice"],MaxPrice = row["MaxPrice"],MinPrice = row["MinPrice"],EndPrice = row["EndPrice"],TradedVolume = row["TradedVolume"],NumberOfTrades = row["NumberOfTrades"])
+            #instead of sending out as bytes, utilizing a custom schema to send as an AvroSchema
+            output = Stock(Mnemonic = row["Mnemonic"],SecurityType = row["SecurityType"],Currency = row["Currency"],
+                           Date = row["Time"],StartPrice	= row["StartPrice"],MaxPrice = row["MaxPrice"],
+                           MinPrice = row["MinPrice"],EndPrice = row["EndPrice"],TradedVolume = row["TradedVolume"],
+                           NumberOfTrades = row["NumberOfTrades"], ProfitOrLoss = 0.0)
             producer.send(output)
